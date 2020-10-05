0ca1d7203eb228f11c7eb74e883b8842836baa37
Swapnil <swapnilsatapathy1998@gmail.com>
2020-10-05 20:57:17 +0530

    Updates

---
+++ precache-manifest.3d55b6499f6b7a755d110c691b8c14fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4095db91f036ffb301163ccd9a6eb0",
+    "url": "/CovidTrackerReact/index.html"
+  },
+  {
+    "revision": "4df32e42e8bec7f1c008",
+    "url": "/CovidTrackerReact/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "811771319aa04a022b63",
+    "url": "/CovidTrackerReact/static/css/main.4f5cbdb5.chunk.css"
+  },
+  {
+    "revision": "4df32e42e8bec7f1c008",
+    "url": "/CovidTrackerReact/static/js/2.42ac3008.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/CovidTrackerReact/static/js/2.42ac3008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811771319aa04a022b63",
+    "url": "/CovidTrackerReact/static/js/main.f528f868.chunk.js"
+  },
+  {
+    "revision": "7f27d054c53c8576d0c4",
+    "url": "/CovidTrackerReact/static/js/runtime-main.800593a3.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/CovidTrackerReact/static/media/logo.6ce9074d.png"
+  }
+]);
